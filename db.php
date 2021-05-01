63a1991390a1bb4c637a9d24fe6c4bc42c7906c0
lu700 <83518257+lu700@users.noreply.github.com>
2021-05-01 19:19:52 -0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$ring=[
+  [
+    "La web de @lucas", // Name
+    "https://pages.7ort.rf.gd/lucas/", // URL
+    "https://app.7ort.rf.gd/assets/avatars/B9kK1JkxOdZl6a55.png", // 1:1 photo
+    "LoL" // Description
+  ]
+]; ?>
